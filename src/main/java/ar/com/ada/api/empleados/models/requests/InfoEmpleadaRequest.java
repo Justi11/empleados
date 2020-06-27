96d5d57762b559a3265dc3ffc2fb0c8c9dbbe72d
Justi11 <justynavarro@gmail.com>
2020-06-27 11:56:04 -0300

    Se armo metodo obtenerPorId en Categoria Service, creamos la clase InfoEmpleadaRequest en carpeta request

---
+++ src/main/java/ar/com/ada/api/empleados/models/requests/InfoEmpleadaRequest.java
@@ -0,0 +1,10 @@
+package ar.com.ada.api.empleados.models.requests;
+
+import java.math.BigDecimal;
+
+public class InfoEmpleadaRequest {
+    public String nombre;
+    public int edad;
+    public BigDecimal sueldo;
+    public int categoriaId;
+}
--- src/main/java/ar/com/ada/api/empleados/services/CategoriaService.java
+++ src/main/java/ar/com/ada/api/empleados/services/CategoriaService.java
@@ -1,12 +1,14 @@
 package ar.com.ada.api.empleados.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ar.com.ada.api.empleados.entities.Categoria;
 import ar.com.ada.api.empleados.repos.CategoriaRepo;
+
 // logica de negocio de las categorias
 @Service
 public class CategoriaService {
@@ -22,4 +24,10 @@ public class CategoriaService {
         return (repo.findAll());
     }
     
+    public Categoria obtenerPorId(int categoriaId){
+        Optional<Categoria> c = repo.findById(categoriaId);
+        if (c.isPresent())
+        return c.get();
+        return null;  
+    }
 }
